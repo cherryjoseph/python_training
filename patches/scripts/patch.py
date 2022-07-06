a38c2262d6c11391d06ebd259937f915b8649ec0
Cheriyan <cherryjoseph@gmail.com>
2022-07-06 11:43:33 +0530

    Extra line after each file and more comments

--- patches/scripts/patch.py
+++ patches/scripts/patch.py
@@ -5,7 +5,7 @@ import glob
 def open_write_content(file):
     with open(file) as curr_file:
         lines=curr_file.readlines()
-        lines.append("\n") # appears after each file
+        lines.append("\n\n") # appears after each file
         with open(parent_location+"\\"+patch_name+"\\"+deploy_script_file_name , "a") as write_file:
             write_file.writelines(lines)
         
@@ -16,20 +16,21 @@ def append_all_files (folder ,extn):
     #print(f"Current working directory : {os.getcwd()} ")
     files_arr= [f for f in glob.glob("*."+extn)]
     files_arr.sort()
-    #print(files_arr)
+    #For esch file found , get the ontent in ppaend mode
     for i in files_arr:
         open_write_content(i)
 
 
 
-
 patch_name ="Jul2022"
 deploy_script_file_name = "deploy_script.sql"
+parent_location = os.getcwd()
 #patch_folder_type =["ddl" ,"dml" ,"views","triggers" , "packages"]
 patch_folder_type =["ddl" , "dml" ,"final" ] # for testing
-parent_location = os.getcwd()
 print(f"Current working directory : {os.getcwd()} ")
-#print("  ")
+
+
+
 for i in patch_folder_type:
     os.chdir(parent_location)
     append_all_files(f"{patch_name}\{i}" , "sql")
